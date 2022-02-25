dc181eda209b0584b2f1c37b7d6a75a056faac3b
unknown <ukaran11@gmail.com>
2022-02-26 02:44:31 +0530

    Utsav | Added test script and basic tests stub

---
+++ app.test.js
@@ -0,0 +1,24 @@
+const request = require('supertest');
+const app = require('./app');
+
+describe('Todos API', () => {
+    it('GET /todos --> array of todos', () => {
+
+    });
+
+    it('GET /todos/:id --> specific todo by ID', () => {
+
+    });
+
+    it('GET /todos/:id --> 404 if not found', () => {
+
+    });
+
+    it('POST /todos --> created todo', () => {
+
+    });
+
+    it('GET /todos --> validates request body', () => {
+
+    });
+});
